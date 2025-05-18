39bf80bb467af4114cf18e8dc9e1862fc673530e
ManonPeeters <manon.peeters@student.uclouvain.be>
2025-05-18 19:42:53 +0200

    dosisier

--- sol.py
+++ sol.py
@@ -16,21 +16,29 @@ def read_numerical_solution(filename):
 
 def compute_truncation_error(numerical, initial_conditions, omega):
     times = numerical[:, 0]
+    h= numerical[1, 0] - numerical[0, 0]  # Pas de temps
     u0x, u0y, v0x, v0y = initial_conditions
     errors = []
 
-    for row in numerical:
-        t, ux_num, uy_num, vx_num, vy_num = row
+    for i  in range(len(times)-1):
+        t, ux_num, uy_num, vx_num, vy_num = numerical[i]
+        t1, ux_num1, uy_num1, vx_num1, vy_num1 = numerical[i+1]
 
         ux_exact, vx_exact = analytical_solution(u0x, v0x, omega, t)
         uy_exact, vy_exact = analytical_solution(u0y, v0y, omega, t)
+        ux_exact1,_= analytical_solution(u0x, v0x, omega, t + h)
+     
+     
 
         err = {
             "time": t,
             "err_ux": abs(ux_num - ux_exact),
             "err_uy": abs(uy_num - uy_exact),
             "err_vx": abs(vx_num - vx_exact),
-            "err_vy": abs(vy_num - vy_exact)
+            "err_vy": abs(vy_num - vy_exact),
+            # "err_t_x": abs((ux_exact1 - ux_exact)/h -vx_exact), 
+            "err": np.sqrt((ux_exact-ux_num)**2 + (uy_exact-uy_num)**2),
+            
         }
         errors.append(err)
 
@@ -49,14 +57,21 @@ def main():
     err_uy = [e['err_uy'] for e in errors]
     err_vx = [e['err_vx'] for e in errors]
     err_vy = [e['err_vy'] for e in errors]
+    # err_t = [e['err_t_x'] for e in errors]
+    errr= [e['err'] for e in errors]
 
     for e in errors:
         print(f"t={e['time']:.5f}", f"err_ux={e['err_ux']:.3e}, err_uy={e['err_uy']:.3e}", 
               f"err_vx={e['err_vx']:.3e}, err_vy={e['err_vy']:.3e}")
     plt.plot(times, err_ux, label="Erreur ux")
     plt.plot(times, err_uy, label="Erreur uy")
+    # plt.plot(times, np.ones(len(times))*np.mean(err_ux), label="Erreur vx")
+    # print("Erreur ux moyenne:", np.mean(err_ux))
     plt.plot(times, err_vx, label="Erreur vx")
+    # plt.plot(times, errr, label="Erreur totale")
     plt.plot(times, err_vy, label="Erreur vy")
+    # plt.plot(times,err_t, label="Erreur troncature", linestyle='-')
+    # plt.plot(times,np.mean(errr)*np.ones(len(times)), label="Erreur moyenne vx", linestyle='--')
     plt.xlabel("Temps [s]")
    
   
@@ -64,7 +79,7 @@ def main():
     plt.title("Erreurs en fonction du temps")
     plt.legend()
     plt.grid()
-    plt.savefig("erreur_pas_troncature_diff_2.pdf")  # Décommentez pour sauvegarder la figure
+    plt.savefig("erreurs.pdf")  # Décommentez pour sauvegarder la figure
     plt.show()
 if __name__ == "__main__":
     main()
